d38d95ec5235a31f9b8bbd54b5aff991a4c0a505
Ibrahim Kalin <hisetkal@gmail.com>
2021-02-27 15:54:53 -0500

    task has been completed

--- test.py
+++ test.py
@@ -7,3 +7,10 @@ number of students = 71
 
 # Here is some useful information I leave for you!
 # Here is some useful information shared
+
+
+
+
+# //Here I add my first line
+# Here is the second line I type
+
